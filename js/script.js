ec1881c8c0a92aa2c52e078a6ac809362ee0ce78
Luciano <luchosr@gmail.com>
2017-03-27 17:28:16 -0300

    se agrego el array de frases

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var quotes = [
+  "\"¡D'oh!!.\" - Homer J. SImpson",
+  "\"Talent is the man in freedom, born in anyone who feels able to fly with their ideas.\" - Luis A. Spinetta",
+  "\"Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.\" - V (V for vendetta)",
+  "\"Dream on big and dare to fail.\" - Norman Vaughan",
+  "\"Quality is not an act, is a habit\" - Aristotle"
+]
