627ce775ecf0bf5d07cfb5a3408b1402283758e1
Luciano <luchosr@gmail.com>
2017-03-28 00:01:17 -0300

    los cambios hasta donde encontre el error

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-var quotes = [
-  "\"¡D'oh!!.\" - Homer J. SImpson",
-  "\"Talent is the man in freedom, born in anyone who feels able to fly with their ideas.\" - Luis A. Spinetta",
-  "\"Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.\" - V (V for vendetta)",
-  "\"Dream on big and dare to fail.\" - Norman Vaughan",
-  "\"Quality is not an act, is a habit\" - Aristotle"
-]
-
-var randomNumber = Math.floor(Math.random() * (quotes.length));
+//alert("funciona el js");
 var newQuote = function (){
+  // alert("testeando!");
+   var randomNumber = Math.floor(Math.random() * (quotes.length));
+  // alert(randomNumber);
+   $(".quotes").text(quotes[randomNumber]);
+   };
+   var quotes = [
+     "\"¡D'oh!!.\" ",
+     "\"Talent is the man in freedom, born in anyone who feels able to fly with their ideas.\"",
+     "\"Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.\"",
+     "\"Dream on big and dare to fail.\"",
+     "\"Quality is not an act, is a habit\""
+   ];
+   var authors = [ "- Homer J. SImpson", "- Luis A. Spinetta",  "- V (V for vendetta)" , " - Norman Vaughan" , " - Aristotle"];
 
-}
+$(document).ready(function(){
+  $(".btn").on("click", newQuote());
+
+
+   });
+   
