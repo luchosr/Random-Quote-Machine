1f24bfb1ad2c5e9bc4f17d3021a90640ff013965
Luciano <luchosr@gmail.com>
2017-03-28 09:49:09 -0300

    arreglado el problema del event listener

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 //alert("funciona el js");
-var newQuote = function (){
-  // alert("testeando!");
-   var randomNumber = Math.floor(Math.random() * (quotes.length));
-  // alert(randomNumber);
-   $(".quotes").text(quotes[randomNumber]);
-   };
    var quotes = [
-     "\"¡D'oh!!.\" ",
+     "\"¡D'oh!!.\"",
      "\"Talent is the man in freedom, born in anyone who feels able to fly with their ideas.\"",
      "\"Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.\"",
      "\"Dream on big and dare to fail.\"",
@@ -14,8 +8,18 @@ var newQuote = function (){
    ];
    var authors = [ "- Homer J. SImpson", "- Luis A. Spinetta",  "- V (V for vendetta)" , " - Norman Vaughan" , " - Aristotle"];
 
+var newQuote = function(){
+  // alert("testeando!");
+   var randomNumber = Math.floor(Math.random() * (quotes.length));
+  // alert(randomNumber);
+   $(".quotes").html(quotes[randomNumber]);
+   $(".author").html(authors[randomNumber]);
+   };
+
 $(document).ready(function(){
-  $(".btn").on("click", newQuote());
+  
+  $("#botn").on("click",newQuote);
+  alert("hola ke ase");
 
 
    });
