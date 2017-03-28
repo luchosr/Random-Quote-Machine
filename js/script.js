c83f72f0e96336a9f7f50c37305debe7d852e319
Luciano <luchosr@gmail.com>
2017-03-28 14:39:16 -0300

    agregados un poco de estilos y mejoras en el funcionamiento

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //alert("funciona el js");
    var quotes = [
-     "\"¡D'oh!!.\"",
-     "\"Talent is the man in freedom, born in anyone who feels able to fly with their ideas.\"",
-     "\"Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.\"",
-     "\"Dream on big and dare to fail.\"",
-     "\"Quality is not an act, is a habit\""
+     "¡D'oh!!.",
+     "Talent is the man in freedom, born in anyone who feels able to fly with their ideas.",
+     "Beneath this mask there is more than flesh. Beneath this mask there is an idea, Mr Creedy. And ideas are bullet-proof!.",
+     "Dream on big and dare to fail.",
+     "Quality is not an act, is a habit.",
+ "Anyone who did something important was disciplined.",
+"Creativity is intelligence having fun.",
+"What you achieve by achieving your goals is not as important as what you become achieving your goals.", "Sometimes you win, sometimes you learn."
    ];
-   var authors = [ "- Homer J. SImpson", "- Luis A. Spinetta",  "- V (V for vendetta)" , " - Norman Vaughan" , " - Aristotle"];
+   var authors = [ "- Homer J. SImpson", "- Luis A. Spinetta",  "- V (V for vendetta)" , " - Norman Vaughan" , " - Aristotle", "- Andrew Hendrixson",
+   "- Albert Einstein", "- Henry David Thoreau","- John Maxwell"];
 
 var newQuote = function(){
   // alert("testeando!");
@@ -19,7 +23,7 @@ var newQuote = function(){
 $(document).ready(function(){
   
   $("#botn").on("click",newQuote);
-  alert("hola ke ase");
+ // alert("hola ke ase");
 
 
    });
